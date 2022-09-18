0cd5b1380d4575d31dd0d7fd92893ecc637fbb4d
Victoria <mariagrisi@cc.ci.ufpb.br>
2022-09-18 17:38:41 -0300

    declaration of constants and statics

--- 3 - Variables, Constants and Statics/constants.rs
+++ 3 - Variables, Constants and Statics/constants.rs
@@ -1,8 +1,11 @@
 // Constants need type annotation
 // You can't change constants values after they're assigned
 // They don't have fixed address in the memory
+// We place constants at the top of the code, outside functions and after module imports/use declarations
+
+const REAL_CONSTANT_DECLARATION: i32 = 12;
 
 fn main(){
     const N: i32 = 5; //"const" is the keyword for constants
-    println!("{}", N);
+    println!("{}, {}", N, REAL_CONSTANT_DECLARATION);
 }
--- 3 - Variables, Constants and Statics/statics.rs
+++ 3 - Variables, Constants and Statics/statics.rs
@@ -1,7 +1,10 @@
 // Statics need type annotation and are also known as "global variables"
 // It has a fixed address in the memory
+// We place statics at the top of the code, outside functions and after module imports/use declarations
+
+static REAL_STATIC_DECLARATION: i32 = 102;
 
 fn main(){
     static N: i32 = 5; //"static" is the keyword for statics
-    println!("{}", N);
+    println!("{}, {}", N, REAL_STATIC_DECLARATION);
 }
