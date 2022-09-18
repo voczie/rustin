c27b78adb66547fd3b6713c634f82ab72cbba778
Victoria <mariagrisi@cc.ci.ufpb.br>
2022-09-18 16:09:15 -0300

    hello world program, just for good luck :)

---
+++ Hello, World! (as always)/hello_world.rs
@@ -0,0 +1,10 @@
+fn main(){ //Fn as in "FuNction"
+    println!("Hello, World!"); //println() is the function to print into the console, "!" makes it a macro (whatever that is)
+}
+
+/*
+"rustc file_name.rs" to compile it!
+
+Unix - "./file_name" to execute it!
+Windows - "./file_name.exe" to execute it!
+*/
